cd6f9ba86e3750556f860a3994e2878292ab89c1
EkkoSonya <353533930@qq.com>
2023-05-07 11:43:05 +0800

    add proxy

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,15 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    proxy: {
+      // 接口地址代理
+      '/chat2doc': {
+        target: 'http://61.169.23.150:8000', // 接口的域名
+        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+        rewrite: path => path.replace(/^\/chat2doc/, '')
+      },
+    }
+  },
 })
